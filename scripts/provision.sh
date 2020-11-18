e8a84962a7f0ccf9cac43eeb6ce58b4ba7f94bef
Noel Forte <dev@noel.forte.is>
2020-11-18 02:18:54 -0500

    one more help text formatting adjustment

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -44,7 +44,7 @@ if [[ ! -e ${DEV_PATH}/config/Caddyfile ]] ; then
   echo "Caddyfile has been copied to ${DEV_PATH}/config/Caddyfile"
   echo -e "\n${GREEN}==> Local environment set up in ${DEV_PATH}.${RESET}\n"
 else
-  echo -e "\n${RED}==> A Caddyfile is already present at ${DEV_PATH}/config/Caddyfile.\n==> To use the one provided by this script, delete the currently existing one and re-run this script.${RESET}\n"
+  echo -e "\n${RED}==> A Caddyfile is already present at ${DEV_PATH}/config/Caddyfile.\n==> To use the one provided by this script, delete the currently existing one\n==> and re-run this script.${RESET}\n"
 fi
 
 read -p "Install LaunchAgent and caddyctl script? (requires root, you may be prompted for a password) (y/n): " do_service_files
@@ -53,7 +53,7 @@ if [[ $do_service_files == "y" || $do_service_files == "yes" ]] ; then
   echo "Making copy of .plist file with changes..."
   sed "s|<WORKING_DIR_PATH>|${DEV_PATH}/config|g" ./LaunchAgents/com.caddyserver.plist > ~/Library/LaunchAgents/com.caddyserver.plist
   
-  echo -e "\n${BOLD}${BLUE}==> Created ~/Library/LaunchAgents/com.caddy.plist. You will need to edit this file in the future to modify the folder path, as this is where Caddy will be launched from.${RESET}\n"
+  echo -e "\n${BOLD}${BLUE}==> Created ~/Library/LaunchAgents/com.caddy.plist.\n==> You will need to edit this file in the future to modify the\n==> directory path, as this is where Caddy will be launched from.${RESET}\n"
   
   echo "Taking root ownership of caddy and moving to /usr/local/sbin..."
   sudo mv $(which caddy) /usr/local/sbin/caddy
@@ -64,8 +64,8 @@ if [[ $do_service_files == "y" || $do_service_files == "yes" ]] ; then
   sed "s|<ROOT_PATH>|${DEV_PATH}|g" ./execs/caddyctl > /usr/local/bin/caddyctl
   chmod +x /usr/local/bin/caddyctl
 
-  echo -e "\n${BOLD}${BLUE}==> Added caddyctl as an executable to /usr/local/bin/caddyctl. You can modify this script at any time by editing this file."
-  echo -e "==> Get help by running 'caddyctl'.${RESET}"
+  echo -e "\n${BOLD}${BLUE}==> Added caddyctl as an executable to /usr/local/bin/caddyctl.\n==> You can modify this script at any time by editing this file."
+  echo -e "==> Get help by running 'caddyctl'.\n${RESET}"
 fi
 
 echo -e "${GREEN}==> Provision complete.${RESET}\n\n"
